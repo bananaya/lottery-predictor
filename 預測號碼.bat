f0070b03a975a72997d2a526a88be1d4790eb619
Tom Yeh <31500573+bananaya@users.noreply.github.com>
2025-07-30 18:48:02 +0800

    Delete 預測號碼.bat

--- 預測號碼.bat
+++
@@ -1,7 +0,0 @@
-curl -X POST https://lotto-api-wslg.onrender.com/api/lottery/predict \
-  -H "Content-Type: application/json" \
-  -d '{
-    "periods": 20,
-    "sheet_name": "我的大樂透資料",
-    "method": "hybrid"
-  }'
